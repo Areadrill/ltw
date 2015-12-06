4783887a8d19ad229293de552b0a7ab2b73fe74f
Areadrill <jackthemotorcycle@gmail.com>
2015-12-06 23:24:59 +0000

    tryna find the problem

--- database/action_createThread.php
+++ database/action_createThread.php
@@ -2,13 +2,13 @@
 require_once('session_check.php');
 if(!isset($_SESSION['username']) || !isset($_POST['threadTitle']) || !isset($_POST['threadText']) || !isset($_POST['eventId'])){
   http_response_code(400);
-  header('Location: ../index.php');
+  
   exit();
 }
 
 if(strlen($_POST['threadTitle']) == 0 || strlen($_POST['threadTitle']) > 1000){
   http_response_code(400);
-  header('Location: ../index.php');
+
   exit();
 }
 
