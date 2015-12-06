5d61202ae4dfa4702457e698c4a51df7a2b5e31e
João Cabral <joaofigueiredocabral@gmail.com>
2015-12-06 15:06:12 +0000

    form validations

---
+++ scripts/formvalidation.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("input[type=text]").attr("pattern", "[A-Za-z0-9 _.,!\"'/$]*").on('invalid', function(e){
+
+    e.target.setCustomValidity("");
+    console.log(e.target.validity.patternMismatch);
+    if(e.target.validity.patternMismatch)
+      e.target.setCustomValidity("Please use only alphanumeric characters");
+  }).on('change', function(event){
+    event.target.setCustomValidity("");
+  });
+
+});
